437c6db9dbd67af7477d6f5cc9cae567d24027ec
mikol <mikolajek.a@gmail.com>
2023-02-07 16:29:36 +0100

    module 194 - providers and listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-          colorSchemeSeed: Color.fromRGBO(96, 150, 180, 1),
-          highlightColor: Color.fromRGBO(147, 191, 207, 1),
-          fontFamily: 'Lato'),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            colorSchemeSeed: Color.fromRGBO(96, 150, 180, 1),
+            highlightColor: Color.fromRGBO(147, 191, 207, 1),
+            fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
